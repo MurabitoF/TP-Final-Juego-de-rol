19637eaafa27d5120af589b8c8d01e6274f3957d
Diego <eserskyd@outlook.com>
2021-05-23 19:03:09 -0300

    Cambiados atributos

--- src/com/company/rooms/Door.java
+++ src/com/company/rooms/Door.java
@@ -3,12 +3,14 @@ package com.company.rooms;
 public class Door {
     private String direction;
     private Room nextRoom;
+    private Room previousRoom;
     private boolean isLocked;
     private String symbol;
 
-    public Door(String direction, Room nextRoom, boolean isLocked, String symbol) {
+    public Door(String direction, Room nextRoom, Room previousRoom, boolean isLocked, String symbol) {
         this.direction = direction;
         this.nextRoom = nextRoom;
+        this.previousRoom = previousRoom;
         this.isLocked = isLocked;
         this.symbol = symbol;
     }
--- src/com/company/rooms/Room.java
+++ src/com/company/rooms/Room.java
@@ -7,27 +7,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
-    private String name;
+    private int id;
+    private String description;
     private Item loot;
     private Player player;
     private Combat combat;
     private List<Door> doors;
 
-    public Room (String name, Item loot, Player player, Combat combat)
+    public Room (int id, String description, Item loot, Player player, Combat combat)
     {
-        this.name = name;
+        this.id = id;
+        this.description = description;
         this.loot = loot;
         this.player = player;
         this.combat = combat;
         this.doors = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Item getLoot() {
